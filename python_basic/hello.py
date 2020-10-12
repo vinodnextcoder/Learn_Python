9dfb055d7fae3335c10767c2ee795b44823878ce
vinod <31404750+vinodnextcoder@users.noreply.github.com>
2020-10-12 11:08:28 +0530

    python basic input

--- python_basic/hello.py
+++ python_basic/hello.py
@@ -7,3 +7,10 @@ print(myNumber2)
   
 myNumber ="helloworld"
 print(myNumber) 
+
+# Python program to illustrate 
+# getting input from user 
+name = input("Enter your name: ")  
+  
+# user entered the name 'harssh' 
+print("hello", name) 
