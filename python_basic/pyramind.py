92614c59554c82b718674e3913cfbdf1234d6338
vinod <31404750+vinodnextcoder@users.noreply.github.com>
2020-10-16 00:17:01 +0530

    avg number

--- python_basic/pyramind.py
+++ python_basic/pyramind.py
@@ -4,6 +4,14 @@ def getInteger():
     result = int(input("Enter integer: ")) 
     return result 
   
+def avgNumber():
+    n=int(input("Enter the number of elements to be inserted: "))
+    a=[]
+    for i in range(0,n):
+        elem=int(input("Enter element: "))
+        a.append(elem)
+    avg=sum(a)/n
+    print("Average of elements in the list",round(avg,2))
 def Main(): 
     print("Started") 
   
@@ -11,6 +19,7 @@ def Main():
     # storing its returned value in the output variable 
     output = getInteger()      
     print(output) 
+    avgNumber()
   
 # now we are required to tell Python  
 # for 'Main' function existence 
