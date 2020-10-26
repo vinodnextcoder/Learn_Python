0a2cdbd2e820e8ed69e9b0b062f99b28ebf7be5c
vinod <31404750+vinodnextcoder@users.noreply.github.com>
2020-10-27 00:05:48 +0530

    List in python

---
+++ algorithm/array/array.py
@@ -0,0 +1,13 @@
+# Lists in Python 
+# Lists are one of the most powerful tools in python. They are just like the arrays declared in other languages.
+#  But the most powerful thing is that list need not be always homogeneous. A single list can contain strings,
+#   integers, as well as objects. Lists can also be used for implementing stacks and queues.
+#  Lists are mutable, i.e., they can be altered once declared.
+# Declaring a list 
+L = [1, "a" , "string" , 1+2] 
+print (L) 
+L.append(6) 
+print (L) 
+L.pop() 
+print (L) 
+print (L[1])
