809abfdcd07daecb4e6d7af14f0f6e573daee72e
vinod <31404750+vinodnextcoder@users.noreply.github.com>
2020-11-07 23:36:43 +0530

    function to sort the array arr[0..n-1]
     Traverse all even positioned elements of input array,

--- algorithm/sorting/waveSort.py
+++ algorithm/sorting/waveSort.py
@@ -15,3 +15,25 @@ arr = [10, 90, 49, 2, 1, 5, 23]
 sortInWave(arr, len(arr)) 
 for i in range(0,len(arr)): 
     print (arr[i]) 
+
+
+
+def sortInWave1(arr, n): 
+      
+    # Traverse all even elements 
+    for i in range(0, n, 2): 
+          
+        # If current even element is smaller than previous 
+        if (i> 0 and arr[i] < arr[i-1]): 
+            print (arr[i-1],arr[i] )
+            arr[i],arr[i-1] = arr[i-1],arr[i] 
+          
+        # If current even element is smaller than next 
+        if (i < n-1 and arr[i] < arr[i+1]): 
+            arr[i],arr[i+1] = arr[i+1],arr[i] 
+  
+# Driver program 
+arr = [10, 90, 49, 2, 1, 5, 23] 
+sortInWave1(arr, len(arr)) 
+for i in range(0,len(arr)): 
+    print (arr[i])
