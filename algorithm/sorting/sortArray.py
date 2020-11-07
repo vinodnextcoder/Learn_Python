9545f7f8338f40416196961c685fc20acd590b89
vinod <31404750+vinodnextcoder@users.noreply.github.com>
2020-11-07 23:40:52 +0530

    array sorting loop

--- algorithm/sorting/sortArray.py
+++ algorithm/sorting/sortArray.py
@@ -9,4 +9,11 @@ while my_list:
     new_list.append(min)
     my_list.remove(min)    
 
+# for i in range (Number):
+#     for j in range(i + 1, Number):
+#         if(NumList[i] > NumList[j]):
+#             temp = NumList[i]
+#             NumList[i] = NumList[j]
+#             NumList[j] = temp
+
 print(new_list)
