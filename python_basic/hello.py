d33e589669172b4fd024a6c41f78d4aae6b12880
vinod <31404750+vinodnextcoder@users.noreply.github.com>
2020-11-07 23:47:18 +0530

    compaound interest

--- python_basic/hello.py
+++ python_basic/hello.py
@@ -8,9 +8,10 @@ print(myNumber2)
 myNumber ="helloworld"
 print(myNumber) 
 
-# Python program to illustrate 
-# getting input from user 
-name = input("Enter your name: ")  
-  
-# user entered the name 'harssh' 
-print("hello", name) 
+princ_amount = float(input(" Please Enter the Principal Amount : "))
+rate_of_int = float(input(" Please Enter the Rate Of Interest   : "))
+time_period = float(input(" Please Enter Time period in Years   : "))
+
+simple_interest = (princ_amount * rate_of_int * time_period) / 100
+
+print("\nSimple Interest for Principal Amount {0} = {1}".format(princ_amount, simple_interest))
