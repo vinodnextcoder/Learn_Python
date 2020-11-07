5e9bf399c369fe91f8d06f7016ec32a5827ebbd5
vinod <31404750+vinodnextcoder@users.noreply.github.com>
2020-11-07 23:40:00 +0530

    Sort a given List in ascending orde

---
+++ algorithm/sorting/sortArray.py
@@ -0,0 +1,12 @@
+my_list = [2,1,4,5]
+new_list = []
+
+while my_list:
+    min = my_list[0]  
+    for x in my_list: 
+        if x < min:
+            min = x
+    new_list.append(min)
+    my_list.remove(min)    
+
+print(new_list)
