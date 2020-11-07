167dff7f19e03c07e0a1c6ae676fc4b13926a309
vinod <31404750+vinodnextcoder@users.noreply.github.com>
2020-11-07 23:50:06 +0530

    calculate series

--- python_basic/hello.py
+++ python_basic/hello.py
@@ -15,3 +15,13 @@ time_period = float(input(" Please Enter Time period in Years   : "))
 simple_interest = (princ_amount * rate_of_int * time_period) / 100
 
 print("\nSimple Interest for Principal Amount {0} = {1}".format(princ_amount, simple_interest))
+
+# Python Program to calculate Sum of Series 1³+2³+3³+….+n³
+import math 
+
+number = int(input("Please Enter any Positive Number  : "))
+total = 0
+
+total = math.pow((number * (number + 1)) /2, 2)
+
+print("The Sum of Series upto {0}  = {1}".format(number, total))
