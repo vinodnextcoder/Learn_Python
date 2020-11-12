3a2983437271a28e1ecc2f62fe8111ad90f0876b
vinod <31404750+vinodnextcoder@users.noreply.github.com>
2020-11-12 10:18:21 +0530

    find maximum

---
+++ python_basic/findMax.py
@@ -0,0 +1,16 @@
+def largest(arr,n): 
+  
+    # return max using max   
+    # inbuilt max() function 
+    return (max(arr)) 
+  
+# Driver Code 
+arr = [10, 324, 45, 90, 9808] 
+n = len(arr) 
+  
+#calculating length of an array 
+  
+Ans = largest(arr,n) 
+  
+#display max 
+print ("Largest in given array is",Ans) 
