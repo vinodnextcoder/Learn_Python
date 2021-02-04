3f1f2d4f353f808a3d6fcbd879d062c6f110f2ce
vinod <31404750+vinodnextcoder@users.noreply.github.com>
2021-02-04 23:53:31 +0530

    print leader in array

---
+++ python_basic/leader.py
@@ -0,0 +1,10 @@
+def printLeader(arr,size):
+    for i in range(0,size):
+        for j in range(i+1,size):
+                if arr[i]<arr[j]:
+                 break
+        if j == size-1:
+            print (arr[i])
+
+arr=[16, 17, 4, 3, 5, 2]  
+printLeader(arr, len(arr))  
